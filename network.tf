e571b780d0d988687a9e8a915260beefacf1bb69
Lakan-Ilaw <134827117+Lakan-Ilaw@users.noreply.github.com>
2023-12-16 08:53:02 -0600

    Create network.tf

---
+++ network.tf
@@ -0,0 +1,42 @@
+locals {
+  azs      = slice(data.aws_availability_zones.available.names, 0, 2)
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.2.0"
+
+  name = var.cluster_name
+  cidr = var.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k+3)]
+  intra_subnets   = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k+6)]
+
+  enable_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+  
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "sec_group" {
+  name_prefix = "${var.cluster_name}-sec_group"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all incoming traffic from any IPv4 address"
+  }
+  
+}
